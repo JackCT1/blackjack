69fa20e4c5e1c3acc3927d2c57ad9b146c8ebfe5
JackCT1 <jackthomas933@gmail.com>
2023-05-25 23:27:52 +0100

    rank to points object added

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+const rankToPoints = {
+  A: 11,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  1: 10,
+  J: 10,
+  Q: 10,
+  K: 10,
+};
+
 function deck() {
   const cards = [];
   const suits = ["S", "D", "C", "H"];
